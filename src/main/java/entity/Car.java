e8b66b3e21a18f6a9df91ea91cc0b65ec70da2fa
Alenka <3980039@gmail.com>
2016-06-30 14:43:33 +0300

    JPA

---
+++ src/main/java/entity/Car.java
@@ -0,0 +1,74 @@
+package entity;
+
+/**
+ * Created by Alenka on 27.06.2016.
+ */
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "cars")
+@NamedQuery(name = "Car.getAll", query = "SELECT c from Car c")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name", length = 32)
+    private String name;      //Название авто
+
+    @Column(name = "date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date releaseDate; //дата выпуска
+
+    @Column(name = "cost")
+    private float cost;       //стоимость
+
+    public Car(String name, Date releaseDate, float cost) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.cost = cost;
+    }
+
+    public Car() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", cost=" + cost +
+                '}';
+    }
+
+}
---
+++ src/main/java/entity/Seller.java
@@ -0,0 +1,53 @@
+package entity;
+
+/**
+ * Created by Alenka on 27.06.2016.
+ */
+public class Seller {
+
+    private String fullName; // Полное имя
+    private int age;         // Возвраст
+    private float salary;    // Зар. плата
+
+    public Seller(String fullName, int age, float salary) {
+        this.fullName = fullName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public Seller() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
